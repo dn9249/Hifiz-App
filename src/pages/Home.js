d580b3704d238de10cc819cce82921c5ba176551
Duy <chipduy@live.unc.edu>
2024-06-02 16:13:29 -0400

    add navbar ui but no routing :(

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import PersistentDrawerLeft from '../components/Navbar'
+
+
+
+function Home() {
+    return(
+        <div>
+            <PersistentDrawerLeft></PersistentDrawerLeft>
+        </div>
+    )
+}
+
+export default Home
